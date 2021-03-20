9d440aa6539736c793c4d5e66ab20cb917efba76
Pooya Parsa <pyapar@gmail.com>
2021-03-20 16:55:00 +0100

    chore: add basic playground

---
+++ playground/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <div class="block">
+      <h2> Reference </h2>
+      <Editor v-model:value="input" />
+    </div>
+    <div class="block">
+      <h2> Schema </h2>
+      <Editor :value="schemaStr" read-only language="json" />
+    </div>
+    <div class="block">
+      <h2> Types </h2>
+      <Editor :value="types" read-only language="typescript" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, computed, watch } from 'vue'
+import { resolveSchema, generateDts } from '../src'
+import Editor from './Editor.vue'
+
+function evaluateSource (src) {
+  let val
+  try {
+    // eslint-disable-next-line no-eval
+    eval('val = ' + src.replace('export default', ''))
+  } catch (err) {}
+  return val
+}
+
+const defaultInput = `export default {
+  name: 'earth',
+  specs: {
+    gravity: {
+      $resolve: val => parseFloat(val),
+      $default: '9.8'
+    },
+    moons: {
+      $resolve: (val = ['moon']) => [].concat(val),
+      $schema: {
+        title: 'planet moons'
+      }
+    }
+  }
+}`
+
+export default defineComponent({
+  components: {
+    Editor
+  },
+  setup () {
+    const input = ref(defaultInput)
+    const schema = ref({})
+    const types = ref('')
+
+    watch(input, (value) => {
+      const inputObj = evaluateSource(value)
+      schema.value = resolveSchema(inputObj)
+      types.value = generateDts(schema.value)
+    })
+
+    const schemaStr = computed(() => JSON.stringify(schema.value, null, 2))
+
+    return {
+      input,
+      schema,
+      schemaStr,
+      types
+    }
+  }
+})
+</script>
+
+<style>
+body, html, #app {
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.container {
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+}
+
+.block {
+  flex: 1;
+  margin: 5px;
+}
+</style>
---
+++ playground/Editor.vue
@@ -0,0 +1,76 @@
+<template>
+  <div ref="editor" class="editor" />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import * as monaco from 'monaco-editor'
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+self.MonacoEnvironment = {
+  getWorker (_, label) {
+    if (label === 'json') {
+      return new JsonWorker()
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return new CssWorker()
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return new HtmlWorker()
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new TsWorker()
+    }
+    return new EditorWorker()
+  }
+}
+
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    language: {
+      type: String,
+      default: 'javascript'
+    },
+    readOnly: {
+      type: Boolean,
+      default: false
+    }
+  },
+  watch: {
+    value (value, oldValue) {
+      if (value !== oldValue && this.readOnly) {
+        this.editor.setValue(value)
+      }
+    }
+  },
+  mounted () {
+    const editor = monaco.editor.create(this.$refs.editor, {
+      value: this.value,
+      language: this.language,
+      readOnly: this.readOnly,
+      minimap: {
+        enabled: false
+      }
+    })
+    this.editor = editor
+    editor.onDidChangeModelContent(() => {
+      this.$emit('update:value', editor.getValue())
+    })
+  }
+})
+</script>
+
+<style scoped>
+.editor {
+  flex: 1;
+  height: 100%;
+}
+</style>
