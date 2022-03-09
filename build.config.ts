c9e0e5622749668bb5be00b3b9e06d9f619dddc5
Pooya Parsa <pyapar@gmail.com>
2022-03-09 13:28:09 +0100

    refactor!: update transform subpath exports

--- build.config.ts
+++ build.config.ts
@@ -1,8 +1,19 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  declaration: true,
+  entries: [
+    './src/index',
+    './src/loader/babel',
+    './src/loader/transform'
+  ],
   rollup: {
     inlineDependencies: true,
     emitCJS: true
-  }
+  },
+  externals: [
+    '@babel/core',
+    '@babel/types',
+    '@babel/standalone'
+  ]
 })
