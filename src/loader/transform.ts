da7979b49e795f763b087326dd2c2ba5a84592af
Pooya Parsa <pooya@pi0.io>
2023-01-03 15:23:14 +0100

    fix: dedefault babel/standalone

--- src/loader/transform.ts
+++ src/loader/transform.ts
@@ -1,5 +1,7 @@
-import { transform as babelTransform } from "@babel/standalone/babel.min.js";
+import * as babel from "@babel/standalone/babel.min.js";
 import untypedPlugin from "./babel";
+
+const { transform: babelTransform } = babel.default || babel;
 
 export function transform(src: string) {
   const res = babelTransform(src, {
