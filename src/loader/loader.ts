4683cae33dd558e833dc2559b08fc79c46e76d70
Pooya Parsa <pooya@pi0.io>
2024-01-19 17:05:51 +0100

    chore: update types

--- src/loader/loader.ts
+++ src/loader/loader.ts
@@ -1,11 +1,9 @@
 import { defu } from "defu";
 import jiti from "jiti";
+import type { JITIOptions } from "jiti";
 import { resolveSchema } from "../schema";
 import type { Schema } from "../types";
 import untypedPlugin from "./babel";
-
-// TODO: https://github.com/unjs/jiti/issues/132
-type JITIOptions = Parameters<typeof jiti>[1];
 
 export interface LoaderOptions {
   jiti?: JITIOptions;
