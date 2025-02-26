2eaceba8f75317d5a13e878e708998c1e0835246
Pooya Parsa <pooya@pi0.io>
2025-02-26 14:51:06 +0100

    build: bundle `@babel/types`

--- build.config.ts
+++ build.config.ts
@@ -1,5 +1,6 @@
 import { defineBuildConfig } from "unbuild";
 import { rm, glob } from "node:fs/promises";
+import { transform } from "esbuild";
 
 export default defineBuildConfig({
   declaration: true,
@@ -12,7 +13,25 @@ export default defineBuildConfig({
   externals: [
     "@babel/core", // This is a type-only dependency
   ],
+  rollup: {
+    inlineDependencies: [
+      "@babel/types",
+      "@babel/helper-validator-identifier",
+      "@babel/helper-string-parser",
+    ],
+  },
   hooks: {
+    "rollup:options"(ctx, opts) {
+      opts.plugins.push({
+        name: "selective-minify",
+        async transform(code, id) {
+          if (id.includes(`node_modules/@babel/`)) {
+            const res = await transform(code, { minify: true });
+            return res.code;
+          }
+        },
+      });
+    },
     async "build:done"() {
       for await (const file of glob("dist/**/*.d.ts")) {
         await rm(file);
